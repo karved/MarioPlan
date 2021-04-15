78792b6eee477819a6b341dd230af6cd4db7f231
karved <dhruvkarve@gmail.com>
2021-04-16 04:22:28 +0530

    Commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,14 +4,7 @@ import 'firebase/auth';
 
  // Your web app's Firebase configuration
  var config = {
-  apiKey: "AIzaSyD98rVyomGmvpf3DaaHi4zuJTo-aJxf-aA",
-  authDomain: "dk-mario-plan.firebaseapp.com",
-  databaseURL: "https://dk-mario-plan.firebaseio.com",
-  projectId: "dk-mario-plan",
-  storageBucket: "dk-mario-plan.appspot.com",
-  messagingSenderId: "493247280150",
-  appId: "1:493247280150:web:2f096512bf1e4ee6673137",
-  measurementId: "G-DEM8JK0LMQ"
+  
 };
 // Initialize Firebase
 firebase.initializeApp(config);
